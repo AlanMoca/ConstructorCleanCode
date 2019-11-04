1d2bcf16a545fba5993fbf00926cb5a11c33bc90
Alan Montes de Oca <moca.alansing@gmail.com>
2019-11-04 01:01:59 -0600

    Revert "Revert "Add Practice Proyect""
    This reverts commit c11176f3e0fbf0f8d45e1a1802d1f0395f976aca.

---
+++ Assets/ConstructorClass/After/Scripts/AutoAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAttack 
+{
+    private double _nextAttackTime;
+    private Entity _owner;
+
+    public AutoAttack( Entity owner ) {                 //CONSTRUCTOR!!! No hereda de Monobehaviour!!! D: D: D:
+        _owner = owner;
+    }
+
+    public void Tick() {
+        if ( CanNotAttack() )
+            return;
+
+         DoAttack();
+    }
+
+    private bool CanNotAttack() {
+
+        if ( Time.time < _nextAttackTime )
+            return true;
+
+        if ( _owner.AttackOn == false )
+            return true;
+
+        if ( _owner.Target == null )
+            return true;
+
+        var distanceToTarget = Vector3.Distance( _owner.transform.position, _owner.Target.transform.position );
+        if ( distanceToTarget > _owner.AttackRange ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void DoAttack() {
+        _nextAttackTime = Time.time + _owner.AttackDelay;
+        _owner.Target.TakeDamage( _owner.AttackDamage );
+    }
+
+}
---
+++ Assets/ConstructorClass/After/Scripts/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int Health = 10;
+    public Entity Target;           //Singleton
+    private AutoAttack _autoAttackController;
+
+    public bool AttackOn { get; set; } = true;
+
+    public double AttackRange { get; set; } = 1f;
+
+    public double AttackDelay { get; set; } = 2f;
+
+    public int AttackDamage { get; set; } = 1;
+
+    
+    void Start()
+    {
+        _autoAttackController = new AutoAttack( this );
+    }
+
+    private void Update() {
+        _autoAttackController.Tick();
+    }
+
+    public void TakeDamage( int amout ) {
+        Health -= amout;
+        if ( Health < 0 ) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Debug.Log( "Dead..." + gameObject.name );
+    }
+
+}
